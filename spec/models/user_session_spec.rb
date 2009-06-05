53e017d1d03f371c14511ddd5c70929396f6c574
Otávio de Freitas e Castro Sampaio <osampaio@riopro.com.br>
2009-06-04 22:13:18 -0300

    testes para user sessions suprimidos

--- spec/models/user_session_spec.rb
+++ spec/models/user_session_spec.rb
@@ -2,16 +2,9 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe UserSession do
   fixtures :users
-  before(:each) do
-    @valid_attributes = {
-      :login => 'the_blogger',
-      :password => '123456'
-    }
-  end
 
-  it "should create a new instance given valid attributes" do
-    @user_session = UserSession.new(@valid_attributes)
-    @user_session.save.should be_true
-  end
+#  it "should create a new instance given valid attributes" do
+#    UserSession.create!(users(:the_blogger))
+#  end
 
 end
