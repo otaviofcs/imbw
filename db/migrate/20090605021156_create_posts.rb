5a351d733f9f2ff97a76af9ea473f170dfccfdf5
Otávio de Freitas e Castro Sampaio <osampaio@riopro.com.br>
2009-06-04 23:19:09 -0300

    criando modelo post

--- db/migrate/20090605021156_create_posts.rb
+++ db/migrate/20090605021156_create_posts.rb
@@ -1,12 +1,12 @@
 class CreatePosts < ActiveRecord::Migration
   def self.up
     create_table :posts do |t|
-      t.string   :title,                                                      :null => false
-      t.text     :body,                                                       :null => false
-      t.text     :body_html,                                                  :null => false
+      t.string   :title,                                              :null => false
+      t.text     :body,                                               :null => false
+      t.text     :body_html,                                          :null => false
       t.boolean  :active,          :default => true,                  :null => false
       t.datetime :published_at
-      t.datetime :edited_at,                                                  :null => false
+      t.datetime :edited_at,                                          :null => false
       t.timestamps
     end
     add_index :posts, :published_at
