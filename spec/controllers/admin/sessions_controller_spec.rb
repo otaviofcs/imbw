b1bca06e7ca01fb57487b6cbe8bb55e2ba3f7ed6
Otávio de Freitas e Castro Sampaio <osampaio@riopro.com.br>
2009-06-04 22:45:19 -0300

    namespace admin

---
+++ spec/controllers/admin/sessions_controller_spec.rb
@@ -0,0 +1,10 @@
+require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
+
+describe Admin::SessionsController do
+
+  #Delete this example and add some real ones
+  it "should use Admin::SessionsController" do
+    controller.should be_an_instance_of(Admin::SessionsController)
+  end
+
+end
