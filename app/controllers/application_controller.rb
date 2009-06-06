444b610c8cad477996cdb8358eea635360788dfc
Otávio de Freitas e Castro Sampaio <osampaio@riopro.com.br>
2009-06-06 00:17:51 -0300

    validações para usuário

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   layout 'application'
 
   helper :all # include all helpers, all the time
-  helper_method :current_user_session, :current_user
+  helper_method :current_user_session, :current_user, :logged_in?
 
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
@@ -42,4 +42,50 @@ class ApplicationController < ActionController::Base
       @current_user = current_user_session && current_user_session.record
     end
 
+    # Check if the user is authorized
+    #
+    # Override this method in your controllers if you want to restrict access
+    # to only a few actions or if you want to check if the user
+    # has the correct rights.
+    #
+    # Example:
+    #
+    #  # only allow nonbobs
+    #  def authorized?
+    #    current_user.login != "bob"
+    #  end
+    #
+    def authorized?(action = action_name, resource = nil)
+      logged_in?
+    end
+
+    # Validates if the current session have a logged user.
+    def user_required
+      unless current_user
+        store_location
+        flash[:notice] = "Você deve estar logado para acessar essa página"
+        redirect_to login_url
+        return false
+      end
+    end
+
+    # Validates if the current session have NO logged user.
+    def no_user_required
+      if current_user
+        store_location
+        flash[:notice] = "Você deve estar deslogado para acessar essa página"
+        redirect_to root_url
+        return false
+      end
+    end
+
+    # Store location to redirect after login
+    def store_location
+      session[:return_to] = request.request_uri
+    end
+
+    def redirect_back_or_default(default)
+      redirect_to(session[:return_to] || default)
+      session[:return_to] = nil
+    end
 end
