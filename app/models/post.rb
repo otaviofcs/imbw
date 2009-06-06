12d0dd2499b92fc43772260193bb01ec8b34c621
Otávio de Freitas e Castro Sampaio <osampaio@riopro.com.br>
2009-06-06 00:51:38 -0300

    testes para post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,12 @@
 class Post < ActiveRecord::Base
 
   named_scope :available, :conditions => ["posts.active = ? and (not posts.published_at is null)", true]
+
+  #
+  # Validations
+  #
+
+  validates_presence_of :edited_at
+  
+
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,10 +3,19 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe Post do
   before(:each) do
     @valid_attributes = {
+      :title => 'my first post',
+      :edited_at => Time.current
     }
+    @post = Post.new(@valid_attributes)
   end
 
   it "should create a new instance given valid attributes" do
-    Post.create!(@valid_attributes)
+    @post.save.should be_true
+  end
+
+  describe "validations" do
+
+    it { should validate_presence_of(:edited_at) }
+
   end
 end
