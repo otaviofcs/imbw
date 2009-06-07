b66d6f71cd28f4e9edfa35d38584886eff1047bb
Otávio de Freitas e Castro Sampaio <osampaio@riopro.com.br>
2009-06-07 02:52:10 -0300

    controller para criação e edição de posts

---
+++ app/controllers/admin/posts_controller.rb
@@ -0,0 +1,25 @@
+class Admin::PostsController < ApplicationController
+
+  # GET /admin/posts/new
+  # Via: new_admin_posts_path
+  # Disponível: [admin]
+  #
+  # novo post
+  def new
+    @post = Post.new
+    @page_title = "Novo Post"
+  end
+
+  # GET /admin/posts/1/edit
+  # Via: edit_admin_posts_path(1)
+  # Disponível: [admin]
+  #
+  # editando post
+  def edit
+    @post = Post.find params[:id]
+    @page_title = "Editando Post ##{@post.id}"
+  end
+
+
+
+end
