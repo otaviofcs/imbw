bf67296b85bf132db3bd19f9a36491f5ffe96d13
Otávio de Freitas e Castro Sampaio <osampaio@riopro.com.br>
2009-06-14 01:35:29 -0300

    validações e criando token

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,10 +1,19 @@
+require 'digest/sha1'
+
 class Gallery < ActiveRecord::Base
 
   belongs_to :user
   has_many :photos, :dependent => :destroy
 
   named_scope :recent, :order => 'id DESC'
+  
+  #
+  # Callbacks
+  #
 
+  def before_validation
+    self.gallery_hash = self.make_token unless self.gallery_hash
+  end
 
   #
   # Options
@@ -14,6 +23,22 @@ class Gallery < ActiveRecord::Base
   # anything else you want your user to change should be added here.
   attr_accessible :title, :description
 
+  #
+  # Validations
+  #
+
   validates_presence_of :title
+  
+  validates_presence_of :gallery_hash
+
+  protected
+
+    def secure_digest(*args)
+      Digest::SHA1.hexdigest(args.flatten.join('--'))
+    end
+
+    def make_token
+      secure_digest(Time.now, (2..11).map{ rand.to_s })
+    end
 
 end
