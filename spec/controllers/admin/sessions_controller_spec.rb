1db38a0e164fa974236e8f102749642d81f6c530
Otávio de Freitas e Castro Sampaio <osampaio@riopro.com.br>
2009-06-15 14:43:14 -0300

    testes e nome da página

--- spec/controllers/admin/sessions_controller_spec.rb
+++ spec/controllers/admin/sessions_controller_spec.rb
@@ -12,37 +12,43 @@ describe Admin::SessionsController do
   end
 
   describe "new" do
+    def call_action(params={})
+      get :new, params
+    end
     it "should get new" do
-      get :new
+      call_action
       response.should be_success
     end
+    it_should_assign_a_page_title
+    it_should_be_accessible_if_not_logged_in
   end
 
   describe "create" do
     before(:each) do
       activate_authlogic
     end
+    def call_action(params={})
+      post :create, params
+    end
     it "should create a -user session with valid login" do
-      post :create, :user_session => { :login => "the_blogger", :password => "123456" }
+      call_action :user_session => { :login => "the_blogger", :password => "123456" }
       UserSession.find.should_not be_nil
       UserSession.find.record.should == users(:the_blogger)
       flash[:success].should_not be_nil
       response.should redirect_to(root_path)
     end
-
     [
       {:login => "the_blogger",        :password => "senha_errada"},
       {:login => "pessoa_inexistente", :password => "123456"}
     ].each do |params_hash|
       it "should not create a -user session with invalid params" do
-        post :create, :user_session => params_hash
+        call_action :user_session => params_hash
         UserSession.find.should be_nil
         flash[:error].should_not be_nil
         response.should_not redirect_to(root_path)
         response.should render_template(:new)
       end
     end
-
   end
 
   describe "destroy" do
