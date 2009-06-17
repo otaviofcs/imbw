336180663d76b02b7b003ec188d694e1974b0f17
Otávio de Freitas e Castro Sampaio <osampaio@riopro.com.br>
2009-06-16 23:53:30 -0300

    freeze do rails

---
+++ vendor/rails/actionpack/lib/action_view/template/handlers/rjs.rb
@@ -0,0 +1,19 @@
+module ActionView
+  module TemplateHandlers
+    class RJS < TemplateHandler
+      include Compilable
+
+      self.default_format = Mime::JS
+
+      def compile(template)
+        "@formats = [:html];" +
+        "controller.response.content_type ||= Mime::JS;" +
+          "update_page do |page|;#{template.source}\nend"
+      end
+
+      def default_format
+        Mime::JS
+      end
+    end
+  end
+end
