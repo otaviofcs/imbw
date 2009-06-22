94a868ef1b04f63cf9940459f4db4de021eb2812
Otávio de Freitas e Castro Sampaio <osampaio@riopro.com.br>
2009-06-21 22:48:29 -0300

    gem twitter para a aplicação

--- config/environment.rb
+++ config/environment.rb
@@ -33,6 +33,8 @@ Rails::Initializer.run do |config|
 
   config.gem "aws-s3", :lib => "aws/s3", :version => ">= 0.6.2"
 
+  config.gem "twitter", :lib => "twitter", :version => ">= 0.6.11"
+
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
