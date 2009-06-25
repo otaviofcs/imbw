04e7356f348f59a4b4a2356bbc61e144bfe58912
Otávio de Freitas e Castro Sampaio <osampaio@riopro.com.br>
2009-06-24 23:41:24 -0300

    tentando resolver estrelas

--- app/helpers/beer_votes_helper.rb
+++ app/helpers/beer_votes_helper.rb
@@ -3,8 +3,10 @@ module BeerVotesHelper
   def star_rating(value)
     link = ""
     link << "<div class='star-rating'>"
-    link << "<span>Voto</span>"
     link << "<ul>"
+    link << "<li style=\"width=#{(value/5)*100}%;\">"
+    link << value.to_s
+    link << "</li>"
     link << "</ul>"
     link << "<div class='clear'></div>"
     link << "</div>"
