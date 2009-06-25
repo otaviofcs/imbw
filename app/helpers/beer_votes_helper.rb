b02bd6fcc5051e3b1323a00eb3c750ef71323c34
Otávio de Freitas e Castro Sampaio <osampaio@riopro.com.br>
2009-06-24 23:29:00 -0300

    helper para star rating

--- app/helpers/beer_votes_helper.rb
+++ app/helpers/beer_votes_helper.rb
@@ -1,2 +1,13 @@
 module BeerVotesHelper
+
+  def star_rating(value)
+    link = ""
+    link << "<div class='star-rating'>"
+    link << "<span>Voto</span>"
+    link << "<ul>"
+    link << "</ul>"
+    link << "<div class='clear'></div>"
+    link << "</div>"
+    link
+  end
 end
