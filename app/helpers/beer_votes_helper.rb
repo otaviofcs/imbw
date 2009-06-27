9312149dbea4a93c4e6f17c08f54fbc8f214c203
Otávio de Freitas e Castro Sampaio <osampaio@riopro.com.br>
2009-06-27 19:00:46 -0300

    helper para estrelas no beer vote. meio marretado ainda. O ideal é css puro...

--- app/helpers/beer_votes_helper.rb
+++ app/helpers/beer_votes_helper.rb
@@ -1,12 +1,20 @@
 module BeerVotesHelper
 
   def star_rating(value)
+    rating_value = value
+    rating_value = 5.0 if value > 5
+    rating_value = 0 if value < 0
     link = ""
     link << "<div class='star-rating'>"
     link << "<ul>"
-    (value.to_i - 5).upto(5) do |star|
+    (6 - rating_value.to_i).upto(5) do
       link << "<li>"
-      link << star.to_s
+      link << image_tag("star-high.gif", :alt => "nota: #{rating_value}", :title => "nota: #{rating_value}")
+      link << "</li>"
+    end
+    if (rating_value * 10 % 10) == 5.0
+      link << "<li>"
+      link << image_tag("star-high-half.gif", :alt => "nota: #{rating_value}", :title => "nota: #{rating_value}")
       link << "</li>"
     end
     link << "</ul>"
