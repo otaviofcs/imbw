8fae035f8cfeb7412723cb6d48c52483cf456f7a
Otávio de Freitas e Castro Sampaio <osampaio@riopro.com.br>
2009-06-29 07:18:48 -0300

    testes para beer_votes controller

--- spec/controllers/beer_votes_controller_spec.rb
+++ spec/controllers/beer_votes_controller_spec.rb
@@ -7,4 +7,17 @@ describe BeerVotesController do
     controller.should be_an_instance_of(BeerVotesController)
   end
 
+  describe "GET /index" do
+    def call_action(params)
+      get :index, params
+    end
+    it_should_be_accessible_if_not_logged_in
+    it_should_assign_a_page_title
+    it "should assign a search variable" do
+      BeerVote.should_receive(:search).and_return(BeerVote)
+      call_action
+      assigns[:search].should_not be_blank
+    end
+  end
+
 end
