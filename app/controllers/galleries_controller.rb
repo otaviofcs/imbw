8c2471a1a9cc48fd74d5b7b947b834c48446f2ff
Otávio de Freitas e Castro Sampaio <osampaio@riopro.com.br>
2009-06-29 07:19:48 -0300

    modificando menu para suportar galleries na página principal

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -1,5 +1,15 @@
 class GalleriesController < ApplicationController
 
+  # GET /beer_votes
+  # GET /
+  # Via: beer_votes_path
+  # Disponível: [todos]
+  #
+  # permite buscar um álbum
+  def index
+    @page_title = "Digite o código do álbum que quer buscar"
+  end
+
   # GET /open/galleries/dfdsfdfdsf56rgrty
   # Via: open_gallery_path(dfdsfdfdsf56rgrty)
   # Disponível: [público]
@@ -8,7 +18,10 @@ class GalleriesController < ApplicationController
   # hash do album
   def show
     @gallery = Gallery.find params[:id]
-    render :text => "você não possui permissão!", :status => 404 unless "#{@gallery.id}-#{@gallery.gallery_hash}" == params[:id]
+    valid = true
+    valid = false unless "#{@gallery.id}-" == params[:id]
+    valid = false unless "#{@gallery.id}-#{@gallery.gallery_hash}" == params[:id]
+    render :text => "você não possui permissão!", :status => 404 unless valid
     @photos = @gallery.photos.recent
     @page_title = "Fotos do álbum #{@gallery.title}"
   end
