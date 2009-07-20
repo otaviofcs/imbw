89fe5a3b159dcef65d4daf6a1f7a5e8cd7d44ea3
Otávio de Freitas e Castro Sampaio <osampaio@riopro.com.br>
2009-07-19 21:28:11 -0300

    atualizando gem twitter

--- config/environment.rb
+++ config/environment.rb
@@ -39,7 +39,7 @@ Rails::Initializer.run do |config|
 
   config.gem "aws-s3", :lib => "aws/s3", :version => ">= 0.6.2"
 
-  config.gem "twitter", :lib => "twitter", :version => ">= 0.6.11"
+  config.gem "twitter", :lib => "twitter", :version => ">= 0.6.12"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
