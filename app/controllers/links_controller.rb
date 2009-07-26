2c09bdc3236cff038dc1e98969be7458e58727d6
Otávio de Freitas e Castro Sampaio <osampaio@riopro.com.br>
2009-07-25 23:07:33 -0300

    estilo e action index para links

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -7,7 +7,9 @@ class LinksController < ApplicationController
   #
   # lista de links
   def index
-    @search = Link.search params[:search]
+    params_search = { :order => :descend_linked_at }
+    params_search = params_search.merge(params[:search]) if params[:search]
+    @search = Link.search params_search
     @links = @search.paginate(:page => params[:page])
     @page_title = "O que estou lendo"
     @tags = Link.tag_counts :limit => 10
