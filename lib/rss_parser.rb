33c37cc3a89723ff3b6a71fbd1bacd0f78e3a279
Otávio de Freitas e Castro Sampaio <osampaio@riopro.com.br>
2009-07-25 21:22:35 -0300

    parser para puxar links do RPLinks

--- lib/rss_parser.rb
+++ lib/rss_parser.rb
@@ -1,42 +1,21 @@
 module RssParser
 
-  class WithUri
-    require 'rss/2.0'
-    require 'open-uri'
-
-    # feed_url = 'http://www.planetrubyonrails.org/xml/rss'
-    def self.parse_rss_feed(feed_url)
-      feed_itens = []
-      open(feed_url) do |http|
-        response = http.read
-        result = RSS::Parser.parse(response, false)
-        output += "Feed Title: #{result.channel.title}<br />"
-        result.items.each_with_index do |item, i|
-          feed_itens << {
-            :title => item.title,
-            :link => item.link,
-            :description => item.description,
-            :linked_at => item.pubDate
-          }
-        end
-      end
-      feed_itens
-    end
-  end
-
   # lib/rss_parser.rb
   class WithReXML
     require 'rexml/document'
 
-    def self.run(url, use_ssl = false)
+    def self.run(host, port, url, use_ssl = false)
 
-      parsed_url = URI.parse(url)
-      request = Net::HTTP::Get.new(parsed_url.path,{"Content-Type"=>"text/xml"})
-      http = Net::HTTP.new(parsed_url.host, parsed_url.port)
+      xml = nil
+      http = Net::HTTP.new(host, port)
       http.use_ssl = use_ssl
-
-      xml = REXML::Document.new http.start {|http| http.request(request) }
-      return
+      http.start do |http|
+          request = Net::HTTP::Get.new(url)
+          response = http.request(request)
+          response.value
+          # turn the results into a REXML document
+          xml = REXML::Document.new(response.body)
+      end
       data = {
         :title    => xml.root.elements['channel/title'].text,
         :home_url => xml.root.elements['channel/link'].text,
