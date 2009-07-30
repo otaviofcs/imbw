14600a431db0cf0b9855d998aa7809e73dc29ede
Otávio de Freitas e Castro Sampaio <osampaio@riopro.com.br>
2009-07-30 15:39:55 -0300

    retornando comentários via ajax

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,7 +1,8 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
-function loadComments(gallery_id) {
-  var url = '/gallery/' + gallery_id + '/comments';
-  Ajax.Updater('comments',  url, { method: 'get' } )
+function loadComments(gallery_id)
+{
+  var url = '/galleries/' + gallery_id + '/comments';
+  new Ajax.Updater('comments',  url, { method: 'get' } );
 }
