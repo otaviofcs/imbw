1fae933ed88929a58d10abeb33931939931527cf
Otávio de Freitas e Castro Sampaio <osampaio@riopro.com.br>
2009-07-30 16:15:01 -0300

    localização e view de comentários

--- app/controllers/gallery_comments_controller.rb
+++ app/controllers/gallery_comments_controller.rb
@@ -9,9 +9,7 @@ class GalleryCommentsController < ApplicationController
   # Comentários de 1 álbum
   def index
 
-    @gallery = Gallery.find params[:gallery_id]
-    @comments = @gallery.comments.all.paginate(:page => params[:page])
-    @new_comment = @gallery.comments.new
+    load_variables
     respond_to do |format|
       format.js
     end
@@ -29,11 +27,22 @@ class GalleryCommentsController < ApplicationController
     respond_to do |format|
       format.js do
         if @success
-          index
+          load_variables
           render :action => 'index'
         end
       end
     end
   end
+
+  protected
+
+    def load_variables
+      @search_params =  { "order" => 'ascend_by_created_at'}
+      @search_params = @search_params.merge(params[:search]) if params[:search]
+      @gallery = Gallery.find params[:gallery_id]
+      @search = @gallery.comments.search(@search_params)
+      @comments = @search.paginate(:page => params[:page])
+      @new_comment = @gallery.comments.new
+    end
 
 end
