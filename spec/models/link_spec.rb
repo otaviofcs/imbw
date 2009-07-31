bee7104cf151227f1788aed9616f5779f2e7db09
Otávio de Freitas e Castro Sampaio <osampaio@riopro.com.br>
2009-07-30 23:34:02 -0300

    link e link spec

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -1,12 +1,25 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Link do
+  fixtures :links
+  
   before(:each) do
     @valid_attributes = {
+      :link => 'http://a.com',
+      :title => 'a link',
+      :linked_at => Time.current
     }
+    @link = Link.new(@valid_attributes)
   end
 
   it "should create a new instance given valid attributes" do
-    Link.create!(@valid_attributes)
+    @link.save.should be_true
+  end
+
+  describe "validations" do
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:linked_at) }
+    it { should validate_presence_of(:link) }
+    it { should validate_uniqueness_of(:link) }
   end
 end
