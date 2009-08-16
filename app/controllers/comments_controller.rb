8691a92ed036ddf57ed2d9d8f2223cd56e49d1ad
Otávio de Freitas e Castro Sampaio <osampaio@riopro.com.br>
2009-08-16 14:54:34 -0300

    busca de comentários

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,7 +7,7 @@ class CommentsController < ApplicationController
   # Comentários de todos os álbuns
   def index
     params_search = { "order" => 'descend_by_id'}
-    params_search = params_search.merge(params[:search]) if params[:search]
+    params_search = params_search.merge(params[:searchlogic_search]) if params[:searchlogic_search]
     @search = Comment.search params_search
     @comments = @search.paginate(:page => params[:page])
     respond_to do |format|
