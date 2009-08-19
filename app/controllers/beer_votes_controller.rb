f593efe15485913d6db5b9b34a955a6884a9776d
Otávio de Freitas e Castro Sampaio <osampaio@riopro.com.br>
2009-08-19 12:02:28 -0300

    link e action para excluir um voto em uma cerveja

--- app/controllers/beer_votes_controller.rb
+++ app/controllers/beer_votes_controller.rb
@@ -1,6 +1,10 @@
 class BeerVotesController < ApplicationController
 
 
+  # Requer um +current_user+ setado.
+  before_filter :user_required, :only => [:destroy]
+
+
   # GET /beer_votes
   # GET /
   # Via: beer_votes_path
@@ -14,6 +18,16 @@ class BeerVotesController < ApplicationController
     @beer_votes = @search.paginate :page => params[:page]
     @page_title = "Cervejas que eu bebi, hoje e ontem - I Might be W.R.O.N.G."
     @user = User.first
+  end
+
+  # DELETE /beer_votes/1
+  # Apaga um voto sobre cerveja
+  def destroy
+    @beer_vote = BeerVote.find params[:id]
+    if @beer_vote.destroy
+      flash[:success] = "Voto em #{@beer_vote.title} destruido com sucesso!"
+    end
+    redirect_to beer_votes_path
   end
 
 end
