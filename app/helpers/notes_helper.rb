7be6bc2ba5dca5515c5fa46ef03db43fb540e3e8
Otávio de Freitas e Castro Sampaio <osampaio@riopro.com.br>
2009-08-26 16:08:51 -0300

    render de hashtags substituindo por links para filtro por tags no imbw

--- app/helpers/notes_helper.rb
+++ app/helpers/notes_helper.rb
@@ -1,25 +1,30 @@
 module NotesHelper
 
-  URL_REGEXP = /[A-Za-z]+:\/\/[A-Za-z0-9-_]+\.[A-Za-z0-9-_:%&\?\/.=]+/
-  TWITTER_USERNAMES_REGEXP = /[@]+[A-Za-z0-9-_]+/
-  TWITTER_HASHTAGS = /[#]+[A-Za-z0-9-_]+/
-
   # baseado na versão para javascript existente em:
   # http://www.simonwhatley.co.uk/examples/twitter/prototype/
   def formatting_twits(twit)
     new_twit = twit
-
+    new_twit = twitter_urls(new_twit)
+    new_twit = twitter_usernames(new_twit)
+    new_twit = twitter_hashtags(new_twit)
+    new_twit
   end
 
   def twitter_urls(twit)
+    /[A-Za-z]+:\/\/[A-Za-z0-9\-_]+\.[A-Za-z0-9\-_:%&\?\/.=]+/
     twit
   end
 
   def twitter_usernames(twit)
+    /@([A-Za-z0-9\-_]+)/
     twit
   end
 
   def twitter_hashtags(twit)
+    hashtags = twit.scan(/#([A-Za-z0-9\-_]+)/i)
+    hashtags.each do |tag|
+      twit = twit.gsub(/##{tag}/, link_to("##{tag}", notes_path(:search => { :tagged_with_on_tags => tag }) ) )
+    end
     twit
   end
 end
--- spec/helpers/notes_helper_spec.rb
+++ spec/helpers/notes_helper_spec.rb
@@ -8,4 +8,10 @@ describe NotesHelper do
     included_modules.should include(NotesHelper)
   end
 
+  describe ".twitter_hashtags(twit)" do
+    it "should render hashtags correctly" do
+      helper.twitter_hashtags("oi, eu sou o #otavio e sou #bacana.").should == "oi, eu sou o <a href=\"/notes?search%5Btagged_with_on_tags%5D%5B%5D=otavio\">#otavio</a> e sou <a href=\"/notes?search%5Btagged_with_on_tags%5D%5B%5D=bacana\">#bacana</a>."
+    end
+  end
+
 end
