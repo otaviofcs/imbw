6ea1a6e1fe6a7b858a38988b0169fdeb449df239
Otávio de Freitas e Castro Sampaio <osampaio@riopro.com.br>
2009-08-26 12:30:36 -0300

    formata notas do twitter colocando urls, hashtags, ... começo

--- app/helpers/notes_helper.rb
+++ app/helpers/notes_helper.rb
@@ -1,2 +1,25 @@
 module NotesHelper
+
+  URL_REGEXP = /[A-Za-z]+:\/\/[A-Za-z0-9-_]+\.[A-Za-z0-9-_:%&\?\/.=]+/
+  TWITTER_USERNAMES_REGEXP = /[@]+[A-Za-z0-9-_]+/
+  TWITTER_HASHTAGS = /[#]+[A-Za-z0-9-_]+/
+
+  # baseado na versão para javascript existente em:
+  # http://www.simonwhatley.co.uk/examples/twitter/prototype/
+  def formatting_twits(twit)
+    new_twit = twit
+
+  end
+
+  def twitter_urls(twit)
+    twit
+  end
+
+  def twitter_usernames(twit)
+    twit
+  end
+
+  def twitter_hashtags(twit)
+    twit
+  end
 end
