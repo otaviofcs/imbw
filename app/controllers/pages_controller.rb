cdf777a19a2fdd07095988a6547484fd0b89142e
Otávio de Freitas e Castro Sampaio <osampaio@riopro.com.br>
2009-08-29 21:09:23 -0300

    mudando mobile true/false

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -15,13 +15,20 @@ class PagesController < ApplicationController
     @recent_activities = @search.paginate(:page => params[:page])
   end
 
-  # GET /pages/mobile
   # GET /pages/mobile/true
   # GET /pages/mobile/false
   # GET /pages/mobile?id=true
+  # GET mobile_pages_path
+  #
+  # alterna entre versão mobile e versão não mobile
   def mobile
-    session[:mobile_view] = false unless params[:id]
-    session[:mobile_view] = params[:id] if params[:id]
+    if params[:id]
+      if params[:id] == "true"
+        session[:mobile_view] = true
+      else
+        session[:mobile_view] = false
+      end
+    end
     redirect_to pages_path
   end
 end
