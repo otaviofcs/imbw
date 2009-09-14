2941b883442732234731edae521f16bb4fce2245
Otávio de Freitas e Castro Sampaio <osampaio@riopro.com.br>
2009-09-14 16:46:15 -0300

    removendo o rails da versão congelada em 2.3.4. Esperando pelo upgrade do servidor

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.3' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -35,7 +35,7 @@ Rails::Initializer.run do |config|
   # paginação
   config.gem 'mislav-will_paginate', :lib => 'will_paginate', :version => '>= 2.3.11', :source => 'http://gems.github.com'
 
-  config.gem 'RedCloth', :lib => 'redcloth', :version => '~> 3.0.4'
+  config.gem 'RedCloth', :lib => 'redcloth', :version => '>= 4.2.2'
 
   config.gem "aws-s3", :lib => "aws/s3", :version => ">= 0.6.2"
 
