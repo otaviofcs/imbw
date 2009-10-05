fc7151348bc272b4d62ec87392ff6e9d6c1ac842
Otávio de Freitas e Castro Sampaio <osampaio@riopro.com.br>
2009-10-04 22:46:22 -0300

    link de links com gsub

--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -1,5 +1,9 @@
 module LinksHelper
 
+  def link_link(link)
+    link_to link.title, link_path( :id => "#{link.id}-#{h( link.title.gsub('.','').gsub(' ','-') )}")
+  end
+
   def link_for_links(link)
     if link.description
       link_to truncate( link.description, :length => 80, :omission => "..."), link.link
