fd0aa2956918e8db4d173389c9cdf996abbc9757
Otávio de Freitas e Castro Sampaio <osampaio@riopro.com.br>
2009-10-12 22:08:51 -0300

    index de cervejas admin

--- app/controllers/admin/beer_votes_controller.rb
+++ app/controllers/admin/beer_votes_controller.rb
@@ -1,7 +1,7 @@
 class Admin::BeerVotesController < AdminController
 
   # GET /beer_votes
-  # Via: beer_votes_path
+  # Via: admin_beer_votes_path
   # Disponível: [todos]
   #
   # lista de beer_votes
@@ -13,9 +13,55 @@ class Admin::BeerVotesController < AdminController
     @page_title = "Cervejas que eu bebi, hoje e ontem - I Might be W.R.O.N.G."
     @user = User.first
   end
+
+  # GET /beer_votes/new
+  # Via: admin_new_beer_vote_path
+  # Disponível: [logados]
+  #
+  # novo voto em cervejas
+  def new
+    @beer_vote = BeerVote.new
+    @page_title = "Nova Cerveja"
+  end
+
+  # GET /beer_votes/1/edit
+  # Via: admin_edit_beer_vote_path(1)
+  # Disponível: [logados]
+  #
+  # editando voto em cervejas
+  def edit
+    @beer_vote = BeerVote.new
+    @page_title = "Editando Cerveja"
+  end
+
+  # POST /beer_votes
+  # Via: admin_beer_votes_path
+  # Disponível: [logados]
+  #
+  # criando voto em cerveja
+  def create
+    @beer_vote = BeerVote.build(params[:beer_vote])
+    if @beer_vote.save
+      flash[:success] = "sucesso"
+    end
+    redirect_to beer_votes_path
+  end
   
+  # PUT /beer_votes/1
+  # Via: admin_beer_vote_path(1)
+  # Disponível: [logados]
+  #
+  # atualizando voto em cerveja
+  def update
+    @beer_vote = BeerVote.find params[:id]
+    if @beer_vote.update_attributes(params[:beer_vote])
+      flash[:success] = "salvo com sucesso!"
+    end
+    redirect_to beer_votes_path
+  end
+
   # DELETE /beer_votes/1
-  # Via: beer_vote_path(1)
+  # Via: admin_beer_vote_path(1)
   # Disponível: [logados]
   #
   # Apaga um voto sobre cerveja
